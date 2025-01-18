0a9eda37b7b2971c363741234db9f3b558c43a08
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2025-01-18 11:58:01 +0530

    logout endpoint

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 import { config } from "../config/app.config";
-import { googleLoginCallback, loginUserController, registerUserController } from "../controllers/auth.controller";
+import { googleLoginCallback, loginUserController, logOutController, registerUserController } from "../controllers/auth.controller";
 
 const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failed`;
 
@@ -9,6 +9,7 @@ const authRoutes = Router();
 
 authRoutes.post("/register", registerUserController);
 authRoutes.post("/login", loginUserController);
+authRoutes.post("/logout", logOutController);
 
 authRoutes.get(
     "/google",
